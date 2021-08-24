f2e8b1e9be39fe4b119b67f912ca4cf47ee4f1ef
Silent-Bard <gabriel.peris.gp@gmail.com>
2021-08-24 15:50:19 -0300

    ready for heroku testing

---
+++ src/main/java/silent_bard/personapi/controller/PersonController.java
@@ -0,0 +1,15 @@
+package silent_bard.personapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class PersonController {
+    
+    @GetMapping
+    public String getBook() {
+        return "Hello, Test!";
+    }
+}
