88ccd5cd5f575ff8c183db796655762d36221798
Silent-Bard <gabriel.peris.gp@gmail.com>
2021-08-24 15:24:20 -0300

    first commit

---
+++ src/test/java/silent_bard/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package silent_bard.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
